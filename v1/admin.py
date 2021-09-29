96f60a15660776002cf4b41110ef92ce890a65a2
selenium_tutorial <leatherback___@dgsw.hs.kr>
2021-09-29 19:28:10 +0900

    [ADD] DjangoAdminForm

--- v1/admin.py
+++ v1/admin.py
@@ -1,4 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+
 from .models import User
 
-admin.site.register(User)
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    list_display = ('email', 'username')
+    list_filter = ('email', 'username')
+    fieldsets = (
+        (None, {"fields": ('email', 'password', 'is_staff', 'is_superuser')}),
+        ('Personal Info', {'fields': ('username',)}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'password1', 'password2', 'is_staff', 'is_superuser')}
+        ),
+    )
+    search_fields = ('email', 'username')
+    ordering = ('email',)
+    filter_horizontal = ()
+
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
