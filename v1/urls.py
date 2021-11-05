8f5caa99d76a1161a20f7144045b4dd98b11b19e
selenium_tutorial <leatherback___@dgsw.hs.kr>
2021-11-05 14:45:59 +0900

    [ADD] Create Group

--- v1/urls.py
+++ v1/urls.py
@@ -18,4 +18,5 @@ urlpatterns = [
     path('user/data/subject/checklist/status/', views.todoListState.as_view(), name='index'),
     path('user/timer/start/', views.startTimer.as_view(), name='index'),
     path('user/timer/stop/', views.stopTimer.as_view(), name='index'),
+    path('user/data/group/', views.groupAPI.as_view(), name='index')
 ]
